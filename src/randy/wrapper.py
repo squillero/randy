eb03af4794442b3fe2a9a5a389f53826561b6ee5
Giovanni Squillero <giovanni.squillero@polito.it>
2021-04-04 15:50:47 +0200

    Add the first working sigma_random

--- src/randy/wrapper.py
+++ src/randy/wrapper.py
@@ -27,99 +27,43 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from typing import Optional
+from typing import Optional, Sequence, Any, Collection, List, Tuple
+import warnings
+import math
 import random
+import numpy as np
+from scipy.stats import truncnorm
 
-class RandomWrapper(random.Random):
+class Randy:
     """Internal random engine to guarantee reproducibility."""
 
-    def __init__(self, seed: Optional = 42):
-        self._generator = random.Random()
+    SMALL_NUMBER = 1e-9
+
+    def __init__(self, seed: Optional = 42) -> None:
+        self._generator = np.random.default_rng(seed)
         self._calls = 0
-        self.seed(seed)
-    def seed(self, *args, **kwargs):
+
+    def seed(self, seed: Any) -> None:
+        """Force a seed value to the internal generator"""
+        warnings.warn("Setting a seed is deprecated. Create a new RandomWrapper instead.", warnings.DeprecationWarning)
+        self._generator = np.random.default_rng(seed)
         self._calls = 0
-        return self._generator.seed(*args, **kwargs)
 
-    def randrange(self, a: int, b: int, *args, **kwargs) -> int:
-        """Alias for randint(a, b-1)"""
-        return self.randint(a, b - 1)
+    @staticmethod
+    def _strength_to_sigma(strength: float):
+        """Stretches [0,1] on [SMALL_NUMBER, 20.723265864228342]."""
 
-    def randint(self, a: int, b: int, loc: Optional[int] = None, strength: Optional[float] = None) -> int:
-        """Return a random integer N such that a <= N <= b. Do consider mean (loc) and strength."""
-        self._calls += 1
-        assert isinstance(a, int) and isinstance(b, int), f"Range must be int"
-        assert a <= b, f"Illegal range [{a}, {b}]"
-        assert loc is None or isinstance(loc, int), f"Mean must be int (found: {type(loc)}"
-        assert loc is None or strength is not None, f"Strength must be specified if loc is used"
-        assert loc is None or a <= loc <= b, f"Illegal loc (loc=f{loc})"
-        assert strength is None or strength == 1 or loc is not None, f"loc must be specified if a strength < 1 is used"
-        assert strength is None or 0 <= strength <= 1, f"Illegal strength (strength={strength})"
+        assert 0 <= strength <= 1, "Invalid sigma (must be [0, 1])"
+        x = (1-strength) / 2 + .5
+        x = min(x, 1-Randy.SMALL_NUMBER)
 
-        if strength is None or strength == 1:
-            # pure random
-            val = self._generator.randint(a, b)
-        elif strength == 0:
-            # deterministic
-            val = loc
-        else:
-            # "true" random
-            scale = strength / (1 - strength**3)
-            scale *= b - a
-            sa, sb = (a - .5 - loc) / scale, (b + .5 - loc) / scale
-
-            # restore n' save numpy random state hoping for reproducibility
-            np_random.set_state(self._np_random)
-            raw_val = truncnorm.rvs(a=sa, b=sb, loc=loc, scale=scale)
-            self._np_random = np_random.get_state()
-
-            val = int(round(raw_val))
-        assert a <= val <= b, f"Stochastic panic: val={val} not in [{a}, {b}]"
+        val = math.log(x / (1 - x))
+        if math.isclose(val, 0): val = Randy.SMALL_NUMBER
         return val
 
-    def random(self, *args, **kwargs):
-        """Proxy for random.random()"""
-        self._calls += 1
-        return self._generator.random(*args, **kwargs)
+    def sigma_random(self, a: Optional[float] = None, b: Optional[float] = None, loc: Optional[float] = None, strength: Optional[float] = None):
+        """Returns a random value in [a, b] biased toward loc with a strength between 0 and 1."""
 
-    def shuffle(self, *args, **kwargs):
-        """Proxy for random.shuffle()"""
-        self._calls += 1
-        return self._generator.shuffle(*args, **kwargs)
-
-    def choice(self,
-               seq: Sequence[Any],
-               last_index: Optional[int] = None,
-               strength: Optional[float] = None,
-               return_index: Optional[bool] = False) -> Any:
-        """Return a random element or its index from the sequence seq. Do consider last_index and strength."""
-        self._calls += 1
-        assert seq, f"seq must be a valid sequence (found: {seq})"
-        assert last_index is None or isinstance(last_index, int), f"last_index must be int (found: {type(last_index)})"
-        assert last_index is None or strength is not None, f"Strength must be specified if last_index is used"
-        assert last_index is None or 0 <= last_index < len(seq), f"Illegal last_index (last_index=f{last_index})"
-        assert strength is None or strength == 1 or last_index is not None, f"last_index must be specified if a strength < 1 is used"
-        assert strength is None or 0 <= strength <= 1, f"Illegal strength (strength={strength})"
-
-        if not last_index or strength == 1:
-            new_index = self._generator.randrange(0, len(seq))
-        elif strength == 0:
-            new_index = last_index
-        else:
-            new_index = self.randint(0, len(seq) - 1, loc=last_index, strength=strength)
-        if return_index:
-            return new_index
-        else:
-            return seq[new_index]
-
-    def group_choice(self, population: Collection[Any], num: float = 1, **kwargs) -> List[Any]:
-        """Proxy for random.choiches()"""
-        self._calls += 1
-        result = [self.choice(population) for _ in range(int(num))]
-        while self.random() < num % 1:
-            result.append(self.choice(population))
-        return result
-
-    def __str__(self):
-        random_state = hex(abs(hash(self._generator.getstate())))
-        return f"{self._calls}:0x{random_state}"
+        std = Randy._strength_to_sigma(strength)
+        clip_a, clip_b = (a - loc) / std, (b - loc) / std
+        return truncnorm.rvs(clip_a, clip_b, loc=loc, scale=std, random_state=self._generator)
